3a0ccead5c01a79531a5f1105c205c1fa0b3dc28
ChonCanCode <163328241+ChonCanCode@users.noreply.github.com>
2026-01-01 16:08:24 +0000

    Adding page step 1 completed

--- pages/Adding.jsx
+++ pages/Adding.jsx
@@ -1,5 +1,49 @@
+import { useState } from "react";
+
 function Adding() {
-  return <>Adding page</>;
+  const [newProperty, setNewProperty] = useState({
+    id: "",
+    street: "",
+    city: "",
+    postcode: "",
+    tenant: "",
+    rent: "",
+    status: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setNewProperty({
+      ...newProperty,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(newProperty);
+  };
+
+  return (
+    <>
+      <div className="p-4">
+        <h2>Add New Property</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="street">Street Name</label>
+            <input
+              type="text"
+              name="street"
+              id="street"
+              value={newProperty.street}
+              onChange={handleInputChange}
+              placeholder="Enter street name"
+            />
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default Adding;
