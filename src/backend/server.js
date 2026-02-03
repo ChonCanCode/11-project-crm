4094f4b3de685725a4a43a7a601da9348362aab7
Chon Lou <163328241+ChonCanCode@users.noreply.github.com>
2026-02-03 15:12:51 +0000

    20260203

--- src/backend/server.js
+++ src/backend/server.js
@@ -2,16 +2,15 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+dotenv.config();
 
 import propertyRoutes from "./routes/properties.js";
 import authRoutes from "./routes/auth.js";
 
-dotenv.config();
-
 const app = express();
-app.use(cors());
-app.use(express.json());
 
+app.use(express.json());
+app.use(cors());
 app.use("/api/properties", propertyRoutes);
 app.use("/api/auth", authRoutes);
 
@@ -21,9 +20,7 @@ app.get("/", (req, res) => {
 
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error(err));
+  .then(() => console.log("MongDB connected"))
+  .catch((err) => console.err(err));
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
-});
+app.listen(5000, console.log("PORT:500 is activated."));
