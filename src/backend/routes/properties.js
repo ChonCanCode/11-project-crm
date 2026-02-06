ba3723ece00d7440794a0a709e1ccd5776264f38
ChonCanCode <163328241+ChonCanCode@users.noreply.github.com>
2026-02-06 16:45:36 +0000

    20260206

--- src/backend/routes/properties.js
+++ src/backend/routes/properties.js
@@ -1,14 +1,13 @@
-const express = require("express");
-const Property = require("../models/Property");
+import express from "express";
+import Property from "../models/Property";
+
 const router = express.Router();
 
-// GET all properties
 router.get("/", async (req, res) => {
   const properties = await Property.find();
   res.json(properties);
 });
 
-// POST new property
 router.post("/", async (req, res) => {
   const property = new Property(req.body);
   await property.save();
